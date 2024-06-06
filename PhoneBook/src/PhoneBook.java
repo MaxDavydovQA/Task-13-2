72a570e05a0171324ccc7d171f22b6863b2237c2
MaxDavydovQA <166731550+MaxDavydovQA@users.noreply.github.com>
2024-06-07 00:08:00 +0300

    Update PhoneBook.java

--- PhoneBook/src/PhoneBook.java
+++ PhoneBook/src/PhoneBook.java
@@ -20,10 +20,10 @@ public class PhoneBook {
 
     public static void main(String[] args) {
         PhoneBook phoneBook = new PhoneBook();
-        phoneBook.add("Ivanov", "123456");
-        phoneBook.add("Petrov", "654321");
-        phoneBook.add("Ivanov", "789101");
-        phoneBook.add("Sidorov", "112233");
+        phoneBook.add("Ivanov", "985123456");
+        phoneBook.add("Petrov", "966654321");
+        phoneBook.add("Ivanov", "977789101");
+        phoneBook.add("Sidorov", "378112233");
 
         System.out.println("Телефоны Иванова: " + phoneBook.get("Ivanov"));
         System.out.println("Телефоны Петрова: " + phoneBook.get("Petrov"));
