27a30e906a0f1fe2fb9d4701333a63439a145c4d
MaxDavydovQA <166731550+MaxDavydovQA@users.noreply.github.com>
2024-06-07 00:06:44 +0300

    Add files via upload

---
+++ PhoneBook/src/PhoneBook.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class PhoneBook {
+    private Map<String, List<String>> phoneBook;
+
+    public PhoneBook() {
+        phoneBook = new HashMap<>();
+    }
+
+    // Метод для добавления записи
+    public void add(String surname, String phoneNumber) {
+        phoneBook.putIfAbsent(surname, new ArrayList<>());
+        phoneBook.get(surname).add(phoneNumber);
+    }
+
+    // Метод для получения номеров по фамилии
+    public List<String> get(String surname) {
+        return phoneBook.getOrDefault(surname, Collections.emptyList());
+    }
+
+    public static void main(String[] args) {
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("Ivanov", "123456");
+        phoneBook.add("Petrov", "654321");
+        phoneBook.add("Ivanov", "789101");
+        phoneBook.add("Sidorov", "112233");
+
+        System.out.println("Телефоны Иванова: " + phoneBook.get("Ivanov"));
+        System.out.println("Телефоны Петрова: " + phoneBook.get("Petrov"));
+        System.out.println("Телефоны Сидорова: " + phoneBook.get("Sidorov"));
+        System.out.println("Телефоны Смирнова: " + phoneBook.get("Smirnov"));
+    }
+}
